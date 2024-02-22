09e29b04f2f751dda24124932e859e62214fa0b4
Kaio Cavalcant <kayollindoso@gmail.com>
2024-02-22 16:34:23 -0300

    Obtendo um usuário por id

--- src/main/java/com/kaio/workshopmongo/services/UserService.java
+++ src/main/java/com/kaio/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.kaio.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kaio.workshopmongo.domain.User;
 import com.kaio.workshopmongo.repository.UserRepository;
+import com.kaio.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User>findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
