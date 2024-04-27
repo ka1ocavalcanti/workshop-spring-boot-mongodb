df4055bf4a4ff691497d957553e1924f523899df
Kaio Cavalcant <kayollindoso@gmail.com>
2024-04-27 20:52:16 -0300

    Referenciando os posts do usuario

--- src/main/java/com/kaio/workshopmongo/config/Instantiation.java
+++ src/main/java/com/kaio/workshopmongo/config/Instantiation.java
@@ -41,8 +41,10 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null,sdf.parse("21/03/2024"),"Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2024"),"Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
