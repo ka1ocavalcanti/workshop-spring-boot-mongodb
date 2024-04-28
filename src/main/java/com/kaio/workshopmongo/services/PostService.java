8933bc5f714c2a104c98a4b027e5544affe2ebb7
Kaio Cavalcant <kayollindoso@gmail.com>
2024-04-27 21:31:23 -0300

    Obtendo um post por id

---
+++ src/main/java/com/kaio/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.kaio.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaio.workshopmongo.domain.Post;
+import com.kaio.workshopmongo.repository.PostRepository;
+import com.kaio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
