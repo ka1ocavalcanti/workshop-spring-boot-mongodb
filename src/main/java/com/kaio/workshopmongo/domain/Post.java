d31e6ba08ece2bb481ef72ee86a7552508f241b0
Kaio Cavalcant <kayollindoso@gmail.com>
2024-04-27 22:02:13 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/kaio/workshopmongo/domain/Post.java
+++ src/main/java/com/kaio/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.kaio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.kaio.workshopmongo.dto.AuthorDTO;
+import com.kaio.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 
@@ -70,6 +75,13 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	public List<CommentDTO> getComments() {
+			return comments;
+		}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -86,8 +98,5 @@ public class Post implements Serializable {
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
-	}
-
-	
-	
+	}	
 }
